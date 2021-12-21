a65fa93f69f4fce470f8947046bd5b8f4b07bc9e
Jebediah <jebediahjoo@protonmail.com>
2021-12-21 16:01:46 +0100

    dotfiles

---
+++ .doom.d/custom.el
@@ -0,0 +1,47 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ansi-color-names-vector
+   ["#2a2426" "#e68183" "#87af87" "#d9bb80" "#89beba" "#e68183" "#87c095" "#e6d6ac"])
+ '(custom-safe-themes
+   '("835868dcd17131ba8b9619d14c67c127aa18b90a82438c8613586331129dda63" "4b6b6b0a44a40f3586f0f641c25340718c7c626cbf163a78b5a399fbe0226659" "6c531d6c3dbc344045af7829a3a20a09929e6c41d7a7278963f7d3215139f6a7" "b186688fbec5e00ee8683b9f2588523abdf2db40562839b2c5458fcfb322c8a4" "246a9596178bb806c5f41e5b571546bb6e0f4bd41a9da0df5dfbca7ec6e2250c" "cbdf8c2e1b2b5c15b34ddb5063f1b21514c7169ff20e081d39cf57ffee89bc1e" "a0be7a38e2de974d1598cf247f607d5c1841dbcef1ccd97cded8bea95a7c7639" "e8df30cd7fb42e56a4efc585540a2e63b0c6eeb9f4dc053373e05d774332fc13" "266ecb1511fa3513ed7992e6cd461756a895dcc5fef2d378f165fed1c894a78c" "a82ab9f1308b4e10684815b08c9cac6b07d5ccb12491f44a942d845b406b0296" "f7fed1aadf1967523c120c4c82ea48442a51ac65074ba544a5aefc5af490893b" "9b54ba84f245a59af31f90bc78ed1240fca2f5a93f667ed54bbf6c6d71f664ac" "da53441eb1a2a6c50217ee685a850c259e9974a8fa60e899d393040b4b8cc922" "4a5aa2ccb3fa837f322276c060ea8a3d10181fecbd1b74cb97df8e191b214313" "7eea50883f10e5c6ad6f81e153c640b3a288cd8dc1d26e4696f7d40f754cc703" "1278c5f263cdb064b5c86ab7aa0a76552082cf0189acf6df17269219ba496053" "4133d2d6553fe5af2ce3f24b7267af475b5e839069ba0e5c80416aa28913e89a" "6c98bc9f39e8f8fd6da5b9c74a624cbb3782b4be8abae8fd84cbc43053d7c175" "8146edab0de2007a99a2361041015331af706e7907de9d6a330a3493a541e5a6" "1d5e33500bc9548f800f9e248b57d1b2a9ecde79cb40c0b1398dec51ee820daf" "7a7b1d475b42c1a0b61f3b1d1225dd249ffa1abb1b7f726aec59ac7ca3bf4dae" "fe2539ccf78f28c519541e37dc77115c6c7c2efcec18b970b16e4a4d2cd9891d" default))
+ '(exwm-floating-border-color "#5b5b5b")
+ '(fci-rule-color "#7c6f64")
+ '(highlight-tail-colors ((("#33312f" "#33312f") . 0) (("#333331" "#333331") . 20)))
+ '(jdee-db-active-breakpoint-face-colors (cons "#0d1011" "#d9bb80"))
+ '(jdee-db-requested-breakpoint-face-colors (cons "#0d1011" "#87af87"))
+ '(jdee-db-spec-breakpoint-face-colors (cons "#0d1011" "#5b5b5b"))
+ '(objed-cursor-color "#e68183")
+ '(pdf-view-midnight-colors (cons "#e6d6ac" "#2a2426"))
+ '(rustic-ansi-faces
+   ["#2a2426" "#e68183" "#87af87" "#d9bb80" "#89beba" "#e68183" "#87c095" "#e6d6ac"])
+ '(vc-annotate-background "#2a2426")
+ '(vc-annotate-color-map
+   (list
+    (cons 20 "#87af87")
+    (cons 40 "#a2b384")
+    (cons 60 "#bdb682")
+    (cons 80 "#d9bb80")
+    (cons 100 "#dcb07e")
+    (cons 120 "#dfa57c")
+    (cons 140 "#e39b7b")
+    (cons 160 "#e3927d")
+    (cons 180 "#e48980")
+    (cons 200 "#e68183")
+    (cons 220 "#e68183")
+    (cons 240 "#e68183")
+    (cons 260 "#e68183")
+    (cons 280 "#c37779")
+    (cons 300 "#a06e6f")
+    (cons 320 "#7d6465")
+    (cons 340 "#7c6f64")
+    (cons 360 "#7c6f64")))
+ '(vc-annotate-very-old-color nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
